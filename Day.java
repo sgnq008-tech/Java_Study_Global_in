b351b4dfc5d0cfaa26b545904fe35726e5b4d81e
sgnq008-tech <sgnq008@gmail.com>
2026-08-08 00:48:39 +0900

    스케줄 관리, 이름 전화번호 등록 프로그램

---
+++ Day.java
@@ -0,0 +1,24 @@
+package com.exam;
+
+public class Day {
+
+		private String work;//하루의 할일을 나타내는 문자열 변수 
+		
+		public void set(String work) {
+			this.work =work;
+			}
+		
+		public String get() {
+			return work;		
+		}
+	
+		public void show() {
+			if(work == null) {System.out.println("없습니다.");}
+			else {System.out.println(work+"입니다.");}
+		}
+	
+	
+	
+	
+	
+}
