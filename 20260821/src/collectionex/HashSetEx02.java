9c8eb682541f757bc44699811e2bab34922e20de
sgnq008-tech <sgnq008@gmail.com>
2026-08-21 18:15:05 +0900

    20260821 TreeMap 학습
     TreeMap 학습

---
+++ 20260821/src/collectionex/HashSetEx02.java
@@ -0,0 +1,62 @@
+package com.collectionex;
+
+import java.util.*;
+class Person{
+	String name;
+	int age;
+	
+	public Person(String name, int age) {
+		
+		this.name = name;
+		this.age = age;
+		
+	}
+	
+	@Override
+	public String toString() {
+		return name+" : "+age;
+		
+	}
+
+}
+
+
+
+public class HashSetEx02 {
+	
+	public static void main(String[] args) {
+		HashSet set = new HashSet();
+		
+		set.add("abc");
+		set.add("abc");
+		set.add(new Person("David",10));
+		set.add(new Person("David",10));
+		System.out.println(set);
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
